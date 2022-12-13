f7f38f606b2429a3ce7d0feed617b0b872a57999
Asad <asad200ana@gmail.com>
2022-12-14 04:45:28 +0530

    alpha support

--- Mindfront/src/com/mime/minefront/graphics/Screen.java
+++ Mindfront/src/com/mime/minefront/graphics/Screen.java
@@ -16,8 +16,15 @@ public class Screen extends Render {
 	}
 
 	public void render() {
-		int anim = (int) (Math.sin(System.currentTimeMillis() % 1000.0 / 1000 * Math.PI * 2) * 200);
-		int anim2 = (int) (Math.cos(System.currentTimeMillis() % 1000.0 / 1000 * Math.PI * 2) * 200);
-		draw(test, (width - 256) / 2 + anim, (height - 256) / 2 + anim2);
+		for (int i = 0; i < width * height; i++) {
+			pixels[i] = 0;
+		}
+
+		for (int i = 0; i < 100; i++) {
+			int anim = (int) (Math.sin((System.currentTimeMillis() + i * 4) % 2000.0 / 2000 * Math.PI * 2) * 200);
+			int anim2 = (int) (Math.cos((System.currentTimeMillis() + i * 4) % 2000.0 / 2000 * Math.PI * 2) * 200);
+			draw(test, (width - 256) / 2 + anim, (height - 256) / 2 - anim2);
+		}
+
 	}
 }
